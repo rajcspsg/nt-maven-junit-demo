cfa14810f18c01a629758e18a68dc14d163ff880
Rajkumar Natarajan <rajkumar.natarajan@ekata.com>
2025-01-26 12:24:37 -0800

    update junit to 5

--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -1,14 +1,12 @@
 package org.example;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.Test;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest extends TestCase
-{
+public class AppTest {
     /**
      * Create the test case
      *
@@ -16,20 +14,12 @@ public class AppTest extends TestCase
      */
     public AppTest( String testName )
     {
-        super( testName );
-    }
-
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
     }
 
     /**
      * Rigourous Test :-)
      */
+    @Test
     public void testApp()
     {
         assertTrue( true );
